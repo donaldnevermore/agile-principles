029cfbae4b705888f8038e7b7dc122f81299a6f8
Donald D. Nevermore <donald33wang@gmail.com>
2021-09-10 20:36:25 +0800

    Add Monostate

---
+++ src/SingletonAndMonostate/Turnstile.cs
@@ -0,0 +1,76 @@
+﻿namespace AgileSoftwareDevelopment.SingletonAndMonostate {
+    public class Turnstile {
+        private static bool isLocked = true;
+        private static bool isAlarming = false;
+        private static int itsCoins = 0;
+        private static int itsRefunds = 0;
+
+        protected static readonly Turnstile Locked = new LockedTurnstile();
+        protected static readonly Turnstile Unlocked = new UnlockedTurnstile();
+        protected static Turnstile itsState = Locked;
+
+        public void Reset() {
+            Lock(true);
+            Alarm(false);
+            itsCoins = 0;
+            itsRefunds = 0;
+            itsState = Locked;
+        }
+
+        public bool IsLocked => isLocked;
+        public bool IsAlarming => isAlarming;
+        public int Coins => itsCoins;
+        public int Refunds => itsRefunds;
+
+        public virtual void Coin() {
+            itsState.Coin();
+        }
+
+        public virtual void Pass() {
+            itsState.Pass();
+        }
+
+        protected void Lock(bool shouldLock) {
+            isLocked = shouldLock;
+        }
+
+        protected void Alarm(bool shouldAlarm) {
+            isAlarming = shouldAlarm;
+        }
+
+        /// <summary>
+        /// Put a coin into the turnstile.
+        /// </summary>
+        public void Deposit() {
+            itsCoins++;
+        }
+
+        public void Refund() {
+            itsRefunds++;
+        }
+
+        internal class LockedTurnstile : Turnstile {
+            public override void Coin() {
+                itsState = Unlocked;
+                Lock(false);
+                Alarm(false);
+                Deposit();
+            }
+
+            public override void Pass() {
+                Alarm(true);
+            }
+        }
+
+        internal class UnlockedTurnstile : Turnstile {
+            public override void Coin() {
+                Refund();
+            }
+
+            public override void Pass() {
+                Lock(true);
+                itsState = Locked;
+            }
+        }
+    }
+}
---
+++ src/SingletonAndMonostate/TurnstileTest.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+namespace AgileSoftwareDevelopment.SingletonAndMonostate {
+    [TestFixture]
+    public class TurnstileTest {
+        [SetUp]
+        public void SetUp() {
+            var t = new Turnstile();
+            t.Reset();
+        }
+
+        [Test]
+        public void TestInit() {
+            var t = new Turnstile();
+            Assert.IsTrue(t.IsLocked);
+            Assert.IsFalse(t.IsAlarming);
+        }
+
+        [Test]
+        public void TestCoin() {
+            var t = new Turnstile();
+            t.Coin();
+            var t1 = new Turnstile();
+            Assert.IsFalse(t1.IsLocked);
+            Assert.IsFalse(t1.IsAlarming);
+            Assert.AreEqual(1, t1.Coins);
+        }
+
+        [Test]
+        public void TestCoinAndPass() {
+            var t = new Turnstile();
+            t.Coin();
+            t.Pass();
+
+            var t1 = new Turnstile();
+            Assert.IsTrue(t1.IsLocked);
+            Assert.IsFalse(t1.IsAlarming);
+            Assert.AreEqual(1, t1.Coins, "coins");
+        }
+
+        [Test]
+        public void TestTwoCoins() {
+            var t = new Turnstile();
+            t.Coin();
+            t.Coin();
+
+            var t1 = new Turnstile();
+            Assert.IsFalse(t1.IsLocked, "unlocked");
+            Assert.AreEqual(1, t1.Coins, "coins");
+            Assert.AreEqual(1, t1.Refunds, "refunds");
+            Assert.IsFalse(t1.IsAlarming);
+        }
+
+        [Test]
+        public void TestPass() {
+            var t = new Turnstile();
+            t.Pass();
+
+            var t1 = new Turnstile();
+            Assert.IsTrue(t1.IsAlarming, "alarm");
+            Assert.IsTrue(t1.IsLocked, "locked");
+        }
+
+        [Test]
+        public void TestCancelAlarm() {
+            var t = new Turnstile();
+            t.Pass();
+            t.Coin();
+
+            var t1 = new Turnstile();
+            Assert.IsFalse(t1.IsAlarming, "alarm");
+            Assert.IsFalse(t1.IsLocked, "locked");
+            Assert.AreEqual(1, t1.Coins, "coins");
+            Assert.AreEqual(0, t1.Refunds, "refunds");
+        }
+
+        [Test]
+        public void TestTwoOperations() {
+            var t = new Turnstile();
+            t.Coin();
+            t.Pass();
+            t.Coin();
+            Assert.IsFalse(t.IsLocked, "unlocked");
+            Assert.AreEqual(2, t.Coins, "coins");
+            t.Pass();
+            Assert.IsTrue(t.IsLocked, "locked");
+        }
+    }
+}
