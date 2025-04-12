3bc2da01d5aef6457259a0727e85e564ca19ae15
dcwong <donald33wang@gmail.com>
2025-04-13 00:38:55 +0800

    Refactor Order

--- src/Ch34_ProxyAndGateway/Order.cs
+++ src/Ch34_ProxyAndGateway/Order.cs
@@ -2,14 +2,6 @@
 
 namespace AgilePrinciples.ProxyAndGateway;
 
-public interface Product {
-    int Price { get; }
-
-    string Name { get; }
-
-    string Sku { get; }
-}
-
 public interface Order {
     string CustomerId { get; }
 
---
+++ src/Ch34_ProxyAndGateway/Product.cs
@@ -0,0 +1,9 @@
+namespace AgilePrinciples.ProxyAndGateway;
+
+public interface Product {
+    int Price { get; }
+
+    string Name { get; }
+
+    string Sku { get; }
+}
